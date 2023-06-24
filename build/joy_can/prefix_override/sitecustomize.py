3d8478454023642e09aa29c9f191eaf497d84059
kou-pen <kou-pen@outlook.jp>
2023-06-24 13:21:34 +0900

    update gui

--- build/joy_can/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kohki/HONROBO_WS/install/joy_can'
--- opencv_test/cam.launch.py
+++ opencv_test/cam.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
                 package='v4l2_camera',
                 executable='v4l2_camera_node',
                 parameters=[
-                    {'video_device': '/dev/video0'},
+                    {'video_device': '/dev/video4'},
                     {'pixel_format': 'YUYV'},
                     {'image_size': [400, 300]},
                     {'time_per_frame': [1, 15]},
