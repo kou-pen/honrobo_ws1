73c427e7f2690252ea6d9a4c1040a6ab5b91494c
tutu-san <tumuri4071.dev@gmail.com>
2023-07-10 18:27:58 +0900

    update example

--- pi_control/pi_lib_data/example.cpp
+++ pi_control/pi_lib_data/example.cpp
@@ -6,6 +6,7 @@ int main(void){
     float motor_pwm = 0.0f;
     float re_tim4 = 0.0f;
 
+    //コントローラーの入力に合わせてかえること
     motor_pid_1.update_target_spd(100.0f);
     /* USER CODE END 2 */
 
@@ -14,15 +15,14 @@ int main(void){
     while (1)
     {
 	    __HAL_TIM_SET_COUNTER(&htim4, 0);
-	    HAL_Delay(10);
+	    HAL_Delay(10); //ここDELTA_Tと同値にする
 
 	    re_tim4 = __HAL_TIM_GET_COUNTER(&htim4);
 
-	    //printf("%f\n\r", re_tim4);
 
-	    motor_pwm = motor_pid_1.motor_calc(re_tim4, MODE_1);
-
-	    printf("%f\n\r", motor_pwm);
+        //0 or 1 を選択して、モーターとエンコーダーの回転方向を合わせること
+	    motor_pwm = motor_pid_1.motor_calc(re_tim4, 0); 
+        //enumで MODE_0 見たくおくといいかもしれないし、何ならDIRECTIONの 0 1 と合わせるように変えてもいいかも
 
 	    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, motor_pwm);
     /* USER CODE END WHILE */
